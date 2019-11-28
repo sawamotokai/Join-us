91c2a46e805dcbd60dbfca36aa68cd8adfbd1b4a
Kai Sawamoto <stayhumblekai@gmail.com>
2019-11-28 12:36:22 -0800

    BULKY DATA INSERT

--- app.js
+++ app.js
@@ -14,14 +14,22 @@ const con = mysql.createConnection({
 // 	if (error) throw error;
 // 	console.log(results);
 // });
-var q = `INSERT INTO users SET ?`;
+// var q = `INSERT INTO users SET ?`;
 
-const person = {
-	email: faker.internet.email(),
-	created_at: faker.date.past()
-};
-con.query(q, person, (error, results) => {
-	if (error) throw error;
+// const person = {
+// 	email: faker.internet.email(),
+// 	created_at: faker.date.past()
+// };
+// con.query(q, person, (error, results) => {
+// 	if (error) throw error;
+// 	console.log(results);
+// });
+
+// inserting bulky data
+var data = [];
+for (let i = 0; i < 768; i++) data.push([ faker.internet.email(), faker.date.past() ]);
+con.query('INSERT INTO users (email, created_at) VALUES ?', [ data ], (err, results) => {
+	if (err) throw err;
 	console.log(results);
 });
 
