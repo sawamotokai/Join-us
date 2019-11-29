8009761684c0b6db98fbce2fda5c16eb271efce9
Kai Sawamoto <stayhumblekai@gmail.com>
2019-11-28 20:48:22 -0800

    style sheet applied

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 dotenv.config();
 
